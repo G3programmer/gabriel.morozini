571828790c8cec4f83f9a2b3ae47ae5f81bef8ef
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-06-18 23:37:57 -0300

    18.06

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-const texto = `Sou um programador em desenvolvimento, grande apreciador de games.
-<br> Amo estar com os meus amigos, com a família e me divertir! 
-<br> E sou um guitarrista por diversão :)`;
+/*Aqui é o textoapresentado no perfil do arquivo html*/
+const texto = `Sou um programador em desenvolvimento, grande apreciador de games, filmes, séries e festivais medievais.
+<br> Amo fazer novos amigos, me divertir com eles é um grande privilégio! Também gosto de passar o tempo com a minha família.<br>
+Aprecio também os momentos em que estou só, assim posso ouvir as minhas músicas enquanto programo.<br> E sou um guitarrista por diversão :)`;
 
 const elemento = document.querySelector(".descricao");
 let index = 0;
